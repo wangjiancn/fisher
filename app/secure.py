b72cdb6ac53c24eee8858c0fc78d55b8a040a047
wangjianchn <wangjianchn@outlook.com>
2018-09-03 14:51:58 +0800

    增加赠送图书

--- app/secure.py
+++ app/secure.py
@@ -1,13 +1,10 @@
 # coding = utf-8
-# 存放数据库密码，账号，Flask key等机密信息
-# 开发环境相关信息
-# 不上传到git服务器
 
 STATUS = 'secure导入成功'
 DEBUG = True
 RUN_PORT = 80
 RUN_HOST = '0.0.0.0'
-SECRET_KEY = ''
+SECRET_KEY = 'RbfokkRe1gsZITyX!Ezxb#Bj&1yxNb1Z'
 
-#数据库相关
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:py123@localhost:3306/fisher'
+# 数据库相关
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/fisher'
