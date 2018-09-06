e7fda5c3b62a7c02ad50db53a3e7c5291d37dbb8
wangjianchn <wangjianchn@outlook.com>
2018-09-07 00:05:59 +0800

    增加验证

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,14 +1,14 @@
 # coding = utf-8
 from wtforms import Form, StringField, IntegerField, PasswordField
-from wtforms.validators import Length, NumberRange, DataRequired, Email, ValidationError
+from wtforms.validators import Length, NumberRange, DataRequired, Email, ValidationError, EqualTo
 from app.models.base import db
 from app.models.user import User
 
 
 class RegisterForm(Form):
     email = StringField(validators=[DataRequired(), Length(8, 64), Email(message="电子邮箱不符合规范")])
-    password = PasswordField(validators=[DataRequired(message='密码不可以为空，请输入你的密码'),Length(6,32)])
-    nickname = StringField(validators=[DataRequired(),Length(2,10,message='昵称至少需要两个字符，最多不超过10个字符')])
+    password = PasswordField(validators=[DataRequired(message='密码不可以为空，请输入你的密码'), Length(6, 32)])
+    nickname = StringField(validators=[DataRequired(), Length(2, 10, message='昵称至少需要两个字符，最多不超过10个字符')])
 
     # todo 自定义验证器，函数以validate_ + 校验对象名 作为名字
     def validate_email(self, field):
@@ -21,6 +21,17 @@ class RegisterForm(Form):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('昵称已被注册')
 
+
 class LoginForm(Form):
     email = StringField(validators=[DataRequired(), Length(8, 64), Email(message="电子邮箱不符合规范")])
-    password = PasswordField(validators=[DataRequired(message='密码不可以为空，请输入你的密码'),Length(6,32)])
+    password = PasswordField(validators=[DataRequired(message='密码不可以为空，请输入你的密码'), Length(6, 32)])
+
+
+class EmailForm(Form):
+    email = StringField(validators=[DataRequired(), Length(8, 64), Email(message="电子邮箱不符合规范")])
+
+
+class ResetPassword(Form):
+    password1 = PasswordField(validators=[DataRequired(message='密码不可以为空，请输入你的密码'), Length(6, 32, message='密码长度为6-32字符'),
+                                          EqualTo('password2', message='两次输入密码不同，请重新输入')])
+    password2 = PasswordField(validators=[DataRequired(message='密码不可以为空，请输入你的密码'), Length(6, 32)])
